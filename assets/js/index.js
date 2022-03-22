3ee6d0a49e51bda4ea9739b8d0b2fd732f66b6a0
Aremu Smog <aremuoluwagbamila@gmail.com>
2022-03-22 08:36:13 +0100

    ao_ffix: stop the document event from propagating to the input field

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,19 @@ const visitor = document.querySelector("#visitor")
 const visitorsInput = document.querySelector("#visitors-name")
 const visitorsNameDisplay = document.querySelector("#visitors-name-display")
 
+visitorsInput.addEventListener("keypress", e => {
+	const key = e.key
+	if (key !== "Enter") {
+		e.stopPropagation()
+	}
+})
+
 document.addEventListener("keypress", e => {
 	const key = e.key
 	if (key === "Enter") {
 		if (visitorsInput.value) {
 			localStorage.setItem("visitorsName", visitorsInput.value)
+			hideVisitor()
 		} else {
 			alert("The input field is empty, you can press `S` to skip")
 		}
@@ -14,12 +22,17 @@ document.addEventListener("keypress", e => {
 
 	if (key === "s" || key === "S") {
 		localStorage.setItem("visitorsName", "there")
+		hideVisitor()
 	}
 })
 
-window.addEventListener("load", () => {
+window.addEventListener("load", async () => {
 	if (localStorage.getItem("visitorsName")) {
-		visitor.style.display = "none"
+		await hideVisitor()
 		visitorsNameDisplay.innerText = localStorage.getItem("visitorsName")
 	}
 })
+
+const hideVisitor = () => {
+	visitor.style.display = "none"
+}
